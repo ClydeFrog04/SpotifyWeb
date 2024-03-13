ce85d339e182f69c64cacdc52346da87ef484ab9
ClydeFrog04 <egan.randal@gmail.com>
2024-03-13 10:57:24 -0600

    better toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,10 +1,11 @@
-import React, {CSSProperties, SetStateAction, useEffect, useRef} from "react";
+import React, {createRef, CSSProperties, SetStateAction, useEffect, useRef} from "react";
 import "./Toast.css";
 
 interface ToastProps {
     toastText: string;
     setShowToast: React.Dispatch<SetStateAction<boolean>>;
     showToast: boolean;
+    startTimeout: boolean;
 
 }
 
@@ -12,31 +13,33 @@ const Toast = (props: ToastProps) =>{
     const TAG = "[Toast.tsx]";
     const toastTTLRef = useRef<NodeJS.Timer>();
     const toastTTL = 5_000;
-    const toastRef = useRef<HTMLElement | null>();
-    // const displayStyle: CSSProperties = {opacity: 1, bottom: "1rem"};
+    const toastRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
 
         setTimeout( () => {
-            toastRef.current = document.getElementById("toastMessage");
+            // toastRef.current = document.getElementById("toastMessage");
             toastRef.current?.classList.add("displayToast");
-
         }, 100);
-
-        setTimeout( () => {
-            toastRef.current?.classList.remove("displayToast");
-            setTimeout( () => {
-                props.setShowToast(false);
-            }, 1000);
-        }, toastTTL);
 
         return  () => {
             clearInterval(toastTTLRef.current as NodeJS.Timer);
         }
     }, []);
 
+    useEffect(() => {
+        if(props.startTimeout === true){
+            setTimeout( () => {
+                toastRef.current?.classList.remove("displayToast");
+                setTimeout( () => {
+                    props.setShowToast(false);
+                }, 1000);
+            }, toastTTL);
+        }
+    }, [props.startTimeout]);
+
     return (
-        <div className="toast" id={"toastMessage"}>
+        <div ref={toastRef} className="toast" id={"toastMessage"}>
             {props.toastText}
         </div>
     );
